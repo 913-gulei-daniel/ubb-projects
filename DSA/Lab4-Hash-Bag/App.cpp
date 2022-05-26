79ba1423adf513c02731cb247148f191c99c5e45
dov36 <daniel.ggradinaru@gmail.com>
2022-05-26 16:48:30 +0300

    added GA lab 5

--- DSA/Lab4-Hash-Bag/App.cpp
+++ DSA/Lab4-Hash-Bag/App.cpp
@@ -1,15 +1,56 @@
-#include "Bag.h"
-#include "ShortTest.h"
-#include "ExtendedTest.h"
 #include <iostream>
-
+#include <cmath>
+#include <cstdlib>
 using namespace std;
 
+
+bool isPrime(int n){
+    if(n == 2) return true;
+    if(n < 2) return false;
+    if(n%2 == 0) return false;
+
+    for(int i = 3; i * i <= n; i+=2){
+        if(n%i == 0)
+            return false;
+
+    }
+
+    return true;
+
+}
+
 int main() {
+    int lmax = 10;
+    int chars = 26;
+    int ct = 0;
+    long long currCombos = 1000000000;
+    chars -= 9;
 
-	testAll();
-	cout << "Short tests over" << endl;
-	testAllExtended();
+    while(currCombos > 1){
+        currCombos /= 2;
+        ct ++;
 
-	cout << "All test over" << endl;
+    }
+
+    currCombos *= 1000000000;
+    chars -= 9;
+
+    while(currCombos > 1){
+        currCombos /= 2;
+        ct ++;
+
+    }
+
+    currCombos *= 100000000;
+
+    while(currCombos > 1){
+        currCombos /= 2;
+        ct ++;
+
+    }
+
+    cout<<ct;
+
+    return 0;
+
 }
